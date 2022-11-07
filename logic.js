a0d9095c174d7d2c7d88a1727c88fc9ac279068d
pitossomo <pedro.h.teles@hotmail.com>
2022-11-07 06:34:39 -0300

    General structure with sections implemented

---
+++ logic.js
@@ -0,0 +1,12 @@
+var mouse;
+var root = document.documentElement;
+
+document.addEventListener('load', (e) => {
+  root.style.setProperty('--base-hue', Math.floor(Math.random()*360))
+})
+
+document.addEventListener('mousemove', (e) => {
+  mouse = {x: e.clientX, y: e.clientY}
+  const newBaseHue = ((mouse.x+mouse.y)/20)%256
+  root.style.setProperty('--base-hue', newBaseHue)
+})
