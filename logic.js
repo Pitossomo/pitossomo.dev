d19f94f10aaeb7a95f24ee4319c6dea0f26608a1
pitossomo <pedro.h.teles@hotmail.com>
2022-11-08 06:21:18 -0300

    Test background colors - not done yet

--- logic.js
+++ logic.js
@@ -1,12 +1,14 @@
 var mouse;
 var root = document.documentElement;
+const INITIAL_HUE = window.getComputedStyle(root).getPropertyValue('--base-hue') // Math.floor(Math.random()*360)
 
 document.addEventListener('load', (e) => {
-  root.style.setProperty('--base-hue', Math.floor(Math.random()*360))
+  root.style.setProperty('--base-hue', INITIAL_HUE)
 })
 
 document.addEventListener('mousemove', (e) => {
   mouse = {x: e.clientX, y: e.clientY}
-  const newBaseHue = ((mouse.x+mouse.y)/100)%256
+  const newBaseHue = INITIAL_HUE + ((mouse.x+mouse.y)/90)
+  console.log(mouse, INITIAL_HUE, newBaseHue)
   root.style.setProperty('--base-hue', newBaseHue)
 })
