53a43344b7c9ba85074cf5746c7626c29db7119d
pitossomo <pedro.h.teles@hotmail.com>
2022-11-26 18:00:09 -0300

    organize web component

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -3,18 +3,41 @@ class ProjectItem extends HTMLElement {
     super();
 
     const shadow = this.attachShadow({ mode: 'open' })
-    shadow.innerHTML = (
-      `<li class="project-item">
-        <figure>
-          <a href="https://healthmoveis.com.br/cob-3136-carrinho-operacional-com-bateria/">
-            <img src="assets/imgs/projects/calculadoraROI.png" alt="Health Móveis ROI Calculator" />
-          </a>
-          <figcaption>Cálculo de ROI para equipamentos hospitalares</figcaption>
-        </figure>
-      </li>
-      <link href="./src/styles/ProjectItem.css" rel="stylesheet" type="text/css">
-      `
-    )
+    shadow.appendChild(this.build())
+    shadow.appendChild(this.styles())
+  }
+
+  build() {
+    const componentRoot = document.createElement('li')
+    componentRoot.setAttribute('class', 'project-item')
+
+    const figure = document.createElement('figure')
+    
+    const a = document.createElement('a')
+    a.setAttribute('href', 'https://healthmoveis.com.br/cob-3136-carrinho-operacional-com-bateria') 
+    
+    const img = document.createElement('img')
+    img.setAttribute('src', 'assets/imgs/projects/calculadoraROI.png')
+    img.setAttribute('alt', 'Health Móveis ROI Calculator')
+    a.appendChild(img)
+    
+    figure.appendChild(a)
+    
+    const figcaption = document.createElement('figcaption')
+    figcaption.innerText = 'Cálculo de ROI para equipamentos hospitalares'
+    figure.appendChild(figcaption)
+    
+    componentRoot.appendChild(figure)
+    
+    return componentRoot
+  }
+
+  styles() {
+    const link = document.createElement('link')
+    link.setAttribute('href','./src/styles/ProjectItem.css')
+    link.setAttribute('rel','stylesheet')
+    link.setAttribute('type','text/css')
+    return link 
   }
 }
 
